c280f032d8322cf8b78ef71312f6090fa4e5f4cc
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-07-25 22:07:46 +0600

    social links section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 import Title from "../subComponents/Title";
 import contactLogo1 from "./../assets/images/Group 1171275317.png";
 import contactLogo2 from "./../assets/images/Group 1171275318.png";
@@ -12,7 +13,7 @@ const Contact = () => {
           "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae sunt adipisci voluptas laborum consequatur nemo fugit exercitationem tenetur!"
         }
       ></Title>
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 w-full">
         <div className="p-5 rounded-xl border flex flex-col col-span-1 h-full gap-5">
           <div className="p-3 rounded-lg bg-green-100 flex flex-col items-start justify-center h-full gap-2">
             <img className="h-10" src={contactLogo1} alt="contact-logo" />
@@ -84,6 +85,41 @@ const Contact = () => {
             className="p-2 w-full bg-red-100/30 placeholder:text-gray-500 text-black text-lg shadow-md rounded-xl sm:min-h-60 min-h-40"
           ></textarea>
         </form>
+      </div>
+      <div className="w-full px-5 lg:py-16 py-10 bg-red-50 mt-10 rounded-xl flex flex-col items-center justify-center">
+        <h3 className="md:text-3xl text-2xl font-semibold text-center">
+          Social Links
+        </h3>
+        <div className="flex items-center gap-5 text-3xl mt-3">
+          <a
+            href="https://x.com/"
+            className="hover:text-sky-500 duration-300"
+            target="_blank"
+          >
+            <FaTwitter></FaTwitter>
+          </a>
+          <a
+            href="https://www.facebook.com/"
+            className="hover:text-blue-500 duration-300"
+            target="_blank"
+          >
+            <FaFacebook></FaFacebook>
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            className="hover:text-pink-500 duration-300"
+            target="_blank"
+          >
+            <FaInstagram></FaInstagram>
+          </a>
+          <a
+            href="https://www.linkedin.com/"
+            className="hover:text-cyan-700 duration-300"
+            target="_blank"
+          >
+            <FaLinkedin></FaLinkedin>
+          </a>
+        </div>
       </div>
     </div>
   );
